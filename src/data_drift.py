f3e805e1844993445a5836178f7c4a2ad953dc49
Faiz Khalifah <faiz_doc@outlook.com>
2025-07-17 07:36:51 +0700

    data drift script

--- src/data_drift.py
+++ src/data_drift.py
@@ -2,17 +2,29 @@ import pandas as pd
 import seaborn as sns
 import mlflow
 import matplotlib.pyplot as plt
+
+
 from sdv.single_table import GaussianCopulaSynthesizer
-from sdv.evaluation.single_table import evaluate_quality
-from sdv.evaluation.single_table import get_column_plot
+from sdv.metadata import SingleTableMetadata
+import pandas as pd
 
 try:
-    df = pd.read_csv('../data/gender.csv')
+    data = pd.read_csv('../data/gender.csv')
     print("Dataset berhasil dimuat.")
 except FileNotFoundError:
     print("Error: Pastikan file berada di direktori yang sama atau telah diunggah.")
     exit()
 
-synthesizer = GaussianCopulaSynthesizer(df)
-synthesizer.fit(data=df)
-synthetic_data = synthesizer.sample(num_rows=500)
+metadata = SingleTableMetadata()
+metadata.detect_from_dataframe(data)
+
+model = GaussianCopulaSynthesizer(metadata)
+model.fit(data)
+synthetic = model.sample(num_rows=len(data))
+
+# save synthetic data
+synthetic.to_csv('../data/synthetic_data.csv', index=False)
+
+#save combined data
+combined = pd.concat([data, synthetic], ignore_index=True)
+combined.to_csv("../data/combined_data.csv", index=False)
